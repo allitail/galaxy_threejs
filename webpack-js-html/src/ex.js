f1dbba581e2c33f8a3640335debadefdabec4b2f
gangyuji <gangyoyul9@gmail.com>
2023-08-21 17:30:16 +0900

    gltf rendering

---
+++ webpack-js-html/src/ex.js
@@ -0,0 +1,10 @@
+import {} from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const gltfLoader = new GLTFLoader();
+gltfLoader.load(
+    '/models/zflip4.gltf',
+    gltf => {
+
+    }
+)
